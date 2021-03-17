48e382ef32a1c6430b2f0bec55186da121479a70
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-03-17 11:44:26 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const data = require('./data/db.json');
 const app = express();
-
 const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.listen(port, (err) => {
     if(err) throw err;
     console.log(`server running on port ${port}`);
@@ -15,4 +18,11 @@ app.get('/', function(req, res) {
 app.get('/movies', function(request, response) {
     response.header('Content-Type', 'application/json');
     response.send(JSON.stringify(data));
+});
+
+app.post('/login', (req, res) => {
+    var user_name = req.body.user;
+    var password = req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    res.json({ "user" : user_name, "password": password});
 });
